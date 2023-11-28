d7d98b7a74abaa0dd6adbbd2fccaf611844ad41f
Kira Solovay <kirasolovay@gmail.com>
2023-11-28 14:32:05 -0500

    blank grid generated

--- script.js
+++ script.js
@@ -12,18 +12,74 @@ for (let difficultyBtn of difficultyBtns){
 }
 
 // randomly generate who is player X (who goes first)
-let playerOne;
+let playerX;
+let playerO;
 if (Math.round(Math.random())=== 0){
-    playerOne = "user"
+    playerX = "user"
+    playerO = "computer"
 }
 else {
-    playerOne = "computer"
+    playerX = "computer"
+    playerO = "user"
 }
-console.log (`${playerOne} goes first.`)
+console.log (`${playerX} goes first as X, ${playerO} goes next as O.`)
 
 // make each of the 9 boxes an object with 3 booleans and a "play" method
+class boxObject {
+    constructor(){
+        this.blank = true;
+        this.x = false;
+        this.o = false;
+        this.value = "blank"
+    }
+    mark(player){
+        if (!this.blank){
+            console.log("You can only mark blank boxes.")
+            return 1;
+        }
+        else{
+            this.blank = false;
+            if (player === playerX){
+                this.x = true;
+                this.value = "x"
+                return 0;
+            }
+            else if (player === playerO){
+                this.o = true;
+                this.value = "x"
+                return 0;
+            }
+        }
+    }
+}
+// create an array with the box objects
+let boxObjects = [];
+
+// create an array containing every div that has a box
+let boxDivs = document.querySelectorAll(".box")
+
+// create an array that contains the button for each box
+let boxButtons = [];
+// assign a button to every box div
+const generateBlankGrid = () => {
+    for (let i = 0; i < 9; i++) {
+        boxObjects[i] = new boxObject
+        let button = document.createElement('button')
+        boxDivs[i].appendChild(button);
+        boxButtons.push(button);
+    }
+}
+
+generateBlankGrid();
+const updateGrid = () => {
+    for(let i = 0; i < 9; i++) {
+        boxButtons[i].textContent = boxObjects[i].value
+    }
+}
+updateGrid();
 
 // determine win conditions
+
 
 // make easy computer turn function
 
