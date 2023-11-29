19580ebe0c4eb83680660763ef871cb2897ae53b
Kira Solovay <kirasolovay@gmail.com>
2023-11-29 11:33:05 -0500

    player mark function created

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-// prompt the user for difficulty level
-const modalContainer = document.getElementById("modal-container")
-const difficultyBtns = document.querySelectorAll(".close-modal")
+// create an in-page chat log
+const chat = document.getElementById("chat");
 
-const toggleClasses = (event) => {
-    modalContainer.classList.toggle('hidden')
-    modalContainer.classList.toggle('shown')
+// create a function that adds chat to the log
+const logChat = (string) => {
+    const p = document.createElement('p');
+    p.textContent = string;
+    chat.appendChild(p)
 }
 
-for (let difficultyBtn of difficultyBtns){
-    difficultyBtn.addEventListener('click', toggleClasses)
+// prompt the user for difficulty level
+const difficultyPrompt = () => {
+    const modalContainer = document.getElementById("modal-container")
+    const difficultyBtns = document.querySelectorAll(".close-modal")
+
+    const toggleClasses = (event) => {
+        modalContainer.classList.toggle('hidden')
+        modalContainer.classList.toggle('shown')
+    }
+    for (let difficultyBtn of difficultyBtns){
+        difficultyBtn.addEventListener('click', toggleClasses)
+    }
 }
 
 // randomly generate who is player X (who goes first)
-let playerX;
-let playerO;
-if (Math.round(Math.random())=== 0){
-    playerX = "user"
-    playerO = "computer"
+const isUserX = () => {
+    if (Math.round(Math.random())=== 0){
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-else {
-    playerX = "computer"
-    playerO = "user"
-}
-console.log (`${playerX} goes first as X, ${playerO} goes next as O.`)
+
 
 // make each of the 9 boxes an object with 3 booleans and a "play" method
 class boxObject {
@@ -30,23 +39,23 @@ class boxObject {
         this.blank = true;
         this.x = false;
         this.o = false;
-        this.value = "blank"
+        this.value = "click to mark"
     }
-    mark(player){
-        if (!this.blank){
-            console.log("You can only mark blank boxes.")
+    mark(xo){
+        if (!this.blank) {
+            logChat("You can only mark blank boxes.");
             return 1;
         }
         else{
             this.blank = false;
-            if (player === playerX){
+            if (xo === 'x'){
                 this.x = true;
-                this.value = "x"
+                this.value = xo
                 return 0;
             }
-            else if (player === playerO){
+            else if (xo === 'o'){
                 this.o = true;
-                this.value = "x"
+                this.value = xo
                 return 0;
             }
         }
@@ -70,21 +79,84 @@ const generateBlankGrid = () => {
     }
 }
 
-generateBlankGrid();
 const updateGrid = () => {
     for(let i = 0; i < 9; i++) {
         boxButtons[i].textContent = boxObjects[i].value
     }
 }
-updateGrid();
 
 // determine win conditions
+const hasWon = (xo) => {
+    if (boxObjects[0].value == boxObjects[1].value == boxObjects[2].value == xo){
+        return true;
+    }
+    else if (boxObjects[3].value == boxObjects[4].value == boxObjects[5].value == xo){
+        return true;
+    }
+    else if (boxObjects[6].value == boxObjects[7].value == boxObjects[8].value == xo){
+        return true;
+    }
+    else if (boxObjects[0].value == boxObjects[3].value == boxObjects[6].value == xo){
+        return true;
+    }
+    else if (boxObjects[1].value == boxObjects[4].value == boxObjects[7].value == xo){
+        return true;
+    }
+    else if (boxObjects[2].value == boxObjects[5].value == boxObjects[8].value == xo){
+        return true;
+    }
+    else if (boxObjects[0].value == boxObjects[4].value == boxObjects[8].value == xo){
+        return true;
+    }
+    else if (boxObjects[2].value == boxObjects[4].value == boxObjects[6].value == xo){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
+const markBox = (xo, boxButton) => {
+    const index = Array.from(boxButtons).indexOf(boxButton);
+    boxObjects[index].mark(xo);
+    boxButton.textContent = boxObjects[index].value;
+    logChat(`Box #${index} has been marked.`);
+};
 
-// make easy computer turn function
+const endTurn = () => {
+    for (let boxButton of boxButtons) {
+        boxButton.removeEventListener('click', boxClickHandler);
+    }
+};
 
-// make medium computer turn function
+const boxClickHandler = (event) => {
+    markBox('x', event.target);
+    endTurn();
+};
 
-// make hard computer turn function
+const userTurn = () => {
+    for (let boxButton of boxButtons) {
+        boxButton.addEventListener('click', boxClickHandler);
+    }
+};
+// make easy function, user X
+// make easy function, computer X
 
-// alternate turns
+// make medium function, user X
+// make medium function, computer X
+
+// make hard function, user X
+// make hard function, computer X
+
+// MAIN
+difficultyPrompt();
+generateBlankGrid();
+updateGrid();
+if(isUserX()){
+    logChat("You go first as X, the computer will go next as O.")
+    userTurn('x');
+}
+else{
+    logChat("The computer goes first as X, you go next as O.")
+}
+
